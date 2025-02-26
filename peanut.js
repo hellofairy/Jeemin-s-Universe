3a3019b3e177b503fb37374bd0785e55da08d05e
jeemin <hellofairy@ewhain.net>
2025-02-26 23:40:11 +0900

    Home 버튼 추가

---
+++ peanut.js
@@ -0,0 +1,8 @@
+// peanut.html
+const index_circle = document.querySelector('.index_circle'); //Jeemin Universe로 돌아가기
+
+// index_circle 클릭 시 index.html로 이동
+index_circle.addEventListener('click', function(event) {
+  event.stopPropagation(); // 부모의 클릭 이벤트를 막음
+  window.location.href = 'index.html';
+});
